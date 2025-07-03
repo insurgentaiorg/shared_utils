c3d8ddd2b0365e84b3f2d31350b9874a6f80b019
nathaninbar <nathaninbar@uvic.ca>
2025-07-02 23:16:56 -0700

    migrated kafka client to use quixstreams>2.0.0 api, bump ver

--- shared_utils/kafka_client.py
+++ shared_utils/kafka_client.py
@@ -1,37 +1,40 @@
 import os
 import json
-from quixstreams import QuixKafkaConsumer, QuixKafkaProducer, QuixKafkaConfiguration
+from quixstreams import Application, ProduceToTopic
 
 class KafkaClient:
     def __init__(self):
-        self.config = QuixKafkaConfiguration(
-            bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"),
-            security_protocol="PLAINTEXT",
-            client_id=os.getenv("KAFKA_CLIENT_ID", "default-client")
-        )
+        self.broker_address = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+        self.client_id = os.getenv("KAFKA_CLIENT_ID", "default-client")
+        self._app = None
         self._producer = None
 
     @property
-    def producer(self):
+    def app(self):
+        if self._app is None:
+            self._app = Application(
+                broker_address=self.broker_address,
+                client_id=self.client_id,
+            )
+        return self._app
+
+    @property
+    def producer(self) -> ProduceToTopic:
         if self._producer is None:
-            self._producer = QuixKafkaProducer(self.config)
+            # this is lazily initialized to a topic-specific producer
+            self._producer = {}
         return self._producer
 
     def send(self, topic: str, value: dict, key: str | None = None):
-        self.producer.publish(
-            topic,
-            key=key,
-            value=json.dumps(value),
-            content_type="application/json"
-        )
+        if topic not in self.producer:
+            topic_obj = self.app.topic(topic)
+            self.producer[topic] = topic_obj.get_producer(value_serializer=json.dumps)
 
-    def create_consumer(self, topic: str, group_id: str) -> QuixKafkaConsumer:
-        return QuixKafkaConsumer(
-            topics=[topic],
-            group_id=group_id,
-            auto_offset_reset="earliest",
-            config=self.config,
-            value_deserializer=lambda m: json.loads(m)
-        )
+        self.producer[topic].produce(value, key=key)
 
-kafka_client = KafkaClient() # module level singleton instance
+    def flush(self):
+        for producer in self.producer.values():
+            producer.flush()
+
+
+kafka_client = KafkaClient()  # module level singleton instance 
