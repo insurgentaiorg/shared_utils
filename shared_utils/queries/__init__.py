b6c57ba8452f71eb4e91a6e04ba3e53f1c8043f6
nathaninbar <nathaninbar@uvic.ca>
2025-07-02 19:15:13 -0700

    added graph sql model and get_graph query

--- shared_utils/queries/__init__.py
+++ shared_utils/queries/__init__.py
@@ -1,2 +1,3 @@
 from .chunks import get_chunk
 from .documents import get_document
+from .graphs import get_graph
--- shared_utils/sql_models/__init__.py
+++ shared_utils/sql_models/__init__.py
@@ -1,3 +1,4 @@
 # exposes all the SQL models in this package
 from .Chunk import Chunk
 from .DocumentMetadata import DocumentMetadata
+from .Graph import Graph
