1e8c1fcd668a1a1cb77e1d90bf968639fd2d234e
nathaninbar <nathaninbar@uvic.ca>
2025-07-03 11:18:20 -0700

    producers now a dict, fix serializer to encode to bytes, ver bump

--- shared_utils/kafka_client.py
+++ shared_utils/kafka_client.py
@@ -7,7 +7,7 @@ class KafkaClient:
         self.broker_address = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
         self.client_id = os.getenv("KAFKA_CLIENT_ID", "default-client")
         self._app = None
-        self._producer = None
+        self._producers = {}  # dict[str, ProduceToTopic]
 
     @property
     def app(self):
@@ -18,23 +18,16 @@ class KafkaClient:
             )
         return self._app
 
-    @property
-    def producer(self) -> ProduceToTopic:
-        if self._producer is None:
-            # this is lazily initialized to a topic-specific producer
-            self._producer = {}
-        return self._producer
-
     def send(self, topic: str, value: dict, key: str | None = None):
-        if topic not in self.producer:
+        if topic not in self._producers:
             topic_obj = self.app.topic(topic)
-            self.producer[topic] = topic_obj.get_producer(value_serializer=json.dumps)
+            serializer = lambda v: json.dumps(v).encode("utf-8")
+            self._producers[topic] = topic_obj.get_producer(value_serializer=serializer)
 
-        self.producer[topic].produce(value, key=key)
+        self._producers[topic].produce(value, key=key)
 
     def flush(self):
-        for producer in self.producer.values():
+        for producer in self._producers.values():
             producer.flush()
 
-
-kafka_client = KafkaClient()  # module level singleton instance 
+kafka_client = KafkaClient()
