273b4eea74ff5d05312b57a00e636bc253638f3a
nathaninbar <nathaninbar@uvic.ca>
2025-07-03 22:33:48 -0700

    added tags field to chunk sql model

--- shared_utils/sql_models/Chunk.py
+++ shared_utils/sql_models/Chunk.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, JSON
 from typing import Optional
 from uuid import UUID
 
@@ -7,8 +8,9 @@ class Chunk(SQLModel, table=True):
     Represents a chunk of a document.
     """
     chunk_id: UUID = Field(primary_key=True, description="The unique identifier for the chunk.")
-    document_id: str = Field(index=True, description="The source document id.")
+    document_id: UUID = Field(foreign_key="documentmetadata.document_id",index=True, description="The source document id.")
     text: str = Field(description="The text content of the chunk.")
+    tags: list[dict] = Field(default_factory=list, ssa_column=Column(JSON), description="List of tags associated with the chunk.")
     #TODO: consider making graph_id an index if performance is an issue
     graph_id : Optional[UUID] = Field(default=None, description="The unique identifier for the graph associated with the chunk.")
     status: str = Field(..., description="The processing status of the chunk, e.g., 'pending', 'processed', 'failed'.")
