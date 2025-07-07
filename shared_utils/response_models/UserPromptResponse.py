94a860985890d75183a3a3f7b93458f57acd32ec
nathaninbar <nathaninbar@uvic.ca>
2025-07-07 11:32:31 -0700

    add ingestion job status response , add comments for statements/referencs for user prompt response

--- shared_utils/response_models/UserPromptResponse.py
+++ shared_utils/response_models/UserPromptResponse.py
@@ -4,5 +4,6 @@ class UserPromptResponse(BaseModel):
     """
     Response model for user prompt operations.
     """
-    prompt: str = Field(..., description="The prompt text provided by the user.")
     response: str = Field(..., description="The response generated based on the user's prompt.")
+    #statements
+    #references
