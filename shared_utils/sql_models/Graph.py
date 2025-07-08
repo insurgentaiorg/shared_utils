86009ba7443ff2f0833c93a13946ed9680bc69ef
nathaninbar <nathaninbar@uvic.ca>
2025-07-08 13:09:17 -0700

    fix sa_column=JSONB -> sa_type=JSONB

--- shared_utils/sql_models/Graph.py
+++ shared_utils/sql_models/Graph.py
@@ -10,6 +10,6 @@ class Graph(SQLModel, table=True):
     graph_id: UUID = Field(primary_key=True, description="The unique identifier for the graph.")
     chunk_id: UUID = Field(foreign_key="chunks.chunk_id", index=True, description="The unique identifier for the chunk associated with the graph.")
 
-    entities: list[str] = Field(default_factory=list, sa_column=JSONB, description="List of entities in the graph.")
-    edges: list[str] = Field(default_factory=list, sa_column=JSONB, description="List of edges in the graph.")
-    relations: list[tuple[str, str, str]] = Field(default_factory=list, sa_column=JSONB, description="List of relations in the graph, represented as tuples of (source_entity, relation_type, target_entity).")
+    entities: list[str] = Field(default_factory=list, sa_type=JSONB, description="List of entities in the graph.")
+    edges: list[str] = Field(default_factory=list, sa_type=JSONB, description="List of edges in the graph.")
+    relations: list[tuple[str, str, str]] = Field(default_factory=list, sa_type=JSONB, description="List of relations in the graph, represented as tuples of (source_entity, relation_type, target_entity).")
