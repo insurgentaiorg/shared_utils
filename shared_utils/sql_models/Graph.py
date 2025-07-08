24553a79627ffcbd0c00ed9e0ddd41b426b976f5
nathaninbar <nathaninbar@uvic.ca>
2025-07-08 12:53:58 -0700

    fix foreign key name for graphs, ver bump 0.2.14

--- shared_utils/sql_models/Graph.py
+++ shared_utils/sql_models/Graph.py
@@ -8,7 +8,7 @@ class Graph(SQLModel, table=True):
     """
     __tablename__ = "chunk_graphs"
     graph_id: UUID = Field(primary_key=True, description="The unique identifier for the graph.")
-    chunk_id: UUID = Field(foreign_key="chunk.chunk_id", index=True, description="The unique identifier for the chunk associated with the graph.")
+    chunk_id: UUID = Field(foreign_key="chunks.chunk_id", index=True, description="The unique identifier for the chunk associated with the graph.")
 
     entities: list[str] = Field(default_factory=list, sa_column=JSONB, description="List of entities in the graph.")
     edges: list[str] = Field(default_factory=list, sa_column=JSONB, description="List of edges in the graph.")
