f650094e2ef76390708eff943bbc5e08696503f5
nathaninbar <nathaninbar@uvic.ca>
2025-07-07 17:40:11 -0700

    rm lazy_proxy in favor of pyi + factory/attr init

--- shared_utils/clients/__init__.py
+++ shared_utils/clients/__init__.py
@@ -1,14 +1,23 @@
-from typing import cast
-from .age_client import AGEClient, create_age_client
+from .mock_client import MockClient, create_mock_client
+mock_client = create_mock_client()
+
 from .db_client import DBClient, create_db_client
 from .redis_client import RedisClient, create_redis_client
 from .s3_client import S3Client, create_s3_client
-from .utils.lazy_proxy import LazyProxy
+from .age_client import AGEClient, create_age_client
 
-# LazyProxy instances for each client
-# This allows the clients to be instantiated only when they are first accessed,
-# casting preserves type hints for IDEs and static analysis tools.
-age_client:AGEClient = cast(AGEClient,LazyProxy(create_age_client))
-db_client:DBClient = cast(DBClient,LazyProxy(create_db_client))
-redis_client:RedisClient = cast(RedisClient,LazyProxy(create_redis_client))
-s3_client:S3Client = cast(S3Client,LazyProxy(create_s3_client))
+_factories = {
+    "db_client": create_db_client,
+    "redis_client": create_redis_client,
+    "s3_client": create_s3_client,
+    "age_client": create_age_client,
+}
+
+_instances = {}
+
+def __getattr__(name: str):
+    if name in _factories:
+        if name not in _instances:
+            _instances[name] = _factories[name]()
+        return _instances[name]
+    raise AttributeError(f"module {__name__} has no attribute {name}")
