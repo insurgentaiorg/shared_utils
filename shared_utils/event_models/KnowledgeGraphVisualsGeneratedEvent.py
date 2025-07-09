d0e17b76c1155afe99b4ecd3c83086ae0f94682a
nathaninbar <nathaninbar@uvic.ca>
2025-07-09 16:13:23 -0700

    add doc id to kg visuals  updated event

--- shared_utils/event_models/KnowledgeGraphVisualsGeneratedEvent.py
+++ shared_utils/event_models/KnowledgeGraphVisualsGeneratedEvent.py
@@ -7,3 +7,4 @@ class KnowledgeGraphVisualsGeneratedEvent(BaseModel):
     This event is used to notify that the visuals for the knowledge graph have been successfully created.
     """
     job_id: UUID = Field(..., description="Unique identifier for the job associated with the document ingestion")
+    document_id: UUID = Field(..., description="The id of the document associated with the generated visuals.")
