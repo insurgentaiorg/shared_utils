728b8137c1a8d8d1706ac78d717568aae2b9fa8e
nathaninbar <nathaninbar@uvic.ca>
2025-07-10 13:52:19 -0700

    fix add postgres vars to db client base

--- shared_utils/clients/utils/db_client_base.py
+++ shared_utils/clients/utils/db_client_base.py
@@ -5,21 +5,22 @@ from typing import Any, ContextManager
 class DBClientBase(ABC):
     """Abstract base class for database clients."""
     def __init__(self):
-        user = getenv("POSTGRES_USER")
-        password = getenv("POSTGRES_PASSWORD")
-        host = getenv("POSTGRES_HOST", "localhost")
-        port = getenv("POSTGRES_PORT", "5432")
-        dbname = getenv("POSTGRES_DB")
+        self.user = getenv("POSTGRES_USER")
+        self.password = getenv("POSTGRES_PASSWORD")
+        self.host = getenv("POSTGRES_HOST", "localhost")
+        self.port = getenv("POSTGRES_PORT", "5432")
+        self.dbname = getenv("POSTGRES_DB")
 
-        if not all([user, password, dbname]):
+        if not all([self.user, self.password, self.dbname]):
             raise EnvironmentError("Missing required PostgreSQL environment variables")
 
+        # psycopg connection parameters
         self.connection_params = {
-            "host": host,
-            "port": port,
-            "dbname": dbname,
-            "user": user,
-            "password": password
+            "host": self.host,
+            "port": self.port,
+            "dbname": self.dbname,
+            "user": self.user,
+            "password": self.password
         }
 
     @abstractmethod
