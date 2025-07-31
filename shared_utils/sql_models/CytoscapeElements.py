18e109f6d2fe00c78eda541c14cfbeb56b0de066
Nathan Inbar <nathaninbar@gmail.com>
2025-07-31 15:40:41 -0700

    add description to cytoscape elements model. remove old layout model

--- shared_utils/sql_models/CytoscapeElements.py
+++ shared_utils/sql_models/CytoscapeElements.py
@@ -14,6 +14,6 @@ class LayoutName(str, Enum):
     MULTIPARTITE = "MULTIPARTITE"
 
 class CytoscapeElements(SQLModel, table=True):
-    concept_graph_name: str = Field(primary_key=True)
-    layout_name: LayoutName = Field(primary_key=True, sa_column_kwargs={"nullable": False})
-    s3_key: str = Field(nullable=True)
+    concept_graph_name: str = Field(primary_key=True, description="The AGE name of the concept graph")
+    layout_name: LayoutName = Field(primary_key=True, sa_column_kwargs={"nullable": False}, description="The graph layout name")
+    s3_key: str = Field(nullable=True, description="the s3 key for the cytoscape json object")
