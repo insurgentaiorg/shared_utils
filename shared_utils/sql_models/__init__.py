2611599beaced97a27e980684fbe99bd92496559
Nathan Inbar <nathaninbar@gmail.com>
2025-07-31 15:55:03 -0700

    rm old layout model from init

--- shared_utils/sql_models/__init__.py
+++ shared_utils/sql_models/__init__.py
@@ -3,5 +3,4 @@ from .Chunk import Chunk
 from .DocumentMetadata import DocumentMetadata
 from .ChunkGraph import ChunkGraph
 from .IngestionJob import IngestionJob
-from .Layout import Layout
 from .CytoscapeElements import CytoscapeElements, LayoutName
